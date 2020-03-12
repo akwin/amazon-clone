bd13066279fa3bb281db9df88c4027fc9cd7ef23
Akanksha Sharma <akankshasharma2415@gmail.com>
2020-03-12 15:14:25 -0700

    filter routes

--- routes/admin.js
+++ routes/admin.js
@@ -2,12 +2,14 @@ const express = require('express');
 
 const router = express.Router();
 
+// /admin/add-product => GET request
 router.get('/add-product', (req, res, next) => {
     //console.log('In add-product middleware!');
-    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Your Product</button></form>');
+    res.send('<form action="/admin/add-product" method="POST"><input type="text" name="title"><button type="submit">Add Your Product</button></form>');
 });
 
-router.post('/product', (req, res, next) => {
+// /admin/add-product => POST request
+router.post('/add-product', (req, res, next) => {
     console.log(req.body);
     res.redirect('/');
 });
