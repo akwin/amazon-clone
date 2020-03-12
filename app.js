fde88598ca48864ee3ab31e02ea69bc2c4ec3f16
Akanksha Sharma <akankshasharma2415@gmail.com>
2020-03-12 14:57:47 -0700

    add 404 error page

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const adminRoutes = require('./routes/admin');
-
 const shopRoutes = require('./routes/shop')
-
-const bodyParser = require('body-parser');
 
 const app = express();
 
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.use(adminRoutes);
-
 app.use(shopRoutes);
 
-app.listen(3000);
+app.use((req, res, next) => {
+    res.status(404).send('<h1> Oops, this page does not exist</h1>');
+});
 
+app.listen(3000);
