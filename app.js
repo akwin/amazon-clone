2b8dcc73ae25e1472c144f86ff4cf103f03cf6c1
Akanksha Sharma <akankshasharma2415@gmail.com>
2020-03-12 12:24:50 -0700

    handle routes using express

--- app.js
+++ app.js
@@ -4,7 +4,12 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => {
+app.use('/add-product', (req, res, next) => {
+    console.log('In another middleware!');
+    res.send('<h1>This is the page to Add Products!</h1>');
+});
+
+app.use('/',  (req, res, next) => {
     console.log('In another middleware!');
     res.send('<h1>Hello from Express!</h1>');
 });
