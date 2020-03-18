b771319bdf745dfb9fa60296bf7d67c6c0370837
Akanksha Sharma <akankshasharma2415@gmail.com>
2020-03-18 12:41:16 -0700

    improve css and file structure

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 
-const adminRoutes = require('./routes/admin');
+const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop')
 
 const app = express();
@@ -10,7 +10,7 @@ const app = express();
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin', adminRoutes);
+app.use('/admin', adminData.router); //adminData refers to all the routes in admin.js file
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
--- routes/admin.js
+++ routes/admin.js
@@ -6,6 +6,8 @@ const rootDir = require('../util/path');
 
 const router = express.Router();
 
+const products = [];
+
 // /admin/add-product => GET request
 router.get('/add-product', (req, res, next) => {
     res.sendFile(path.join(rootDir, 'views', 'add-product.html'));    
@@ -13,8 +15,9 @@ router.get('/add-product', (req, res, next) => {
 
 // /admin/add-product => POST request
 router.post('/add-product', (req, res, next) => {
-    console.log(req.body);
+    products.push({ title: req.body.title });
     res.redirect('/');
 });
 
-module.exports = router;
+exports.router = router;
+exports.products = products;
