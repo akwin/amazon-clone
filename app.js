55fe8b76b38d2799dea02707f679059853784967
Akanksha Sharma <akankshasharma2415@gmail.com>
2020-03-24 18:07:19 -0700

    incorporating MVC pattern

--- app.js
+++ app.js
@@ -3,8 +3,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 //const expressHbs = require('express-handlebars');
 
-const adminRoutes = require('./routes/admin');
-const shopRoutes = require('./routes/shop')
+const errorController = require('./controllers/error');
 
 const app = express();
 
@@ -12,14 +11,15 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/admin', adminRoutes); //adminData refers to all the exports in admin.js file
 app.use(shopRoutes);
 
-app.use((req, res, next) => {
-    res.status(404).render('error', {docTitle:'Page Not Found'}, );
-});
+app.use(errorController.get404);
 
 app.listen(3000);
