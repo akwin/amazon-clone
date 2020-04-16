c4f3a67fb43aaedbe68434b7ae1244c3eab4b5de
Akanksha Sharma <akankshasharma2415@gmail.com>
2020-04-15 18:21:46 -0700

    add pagination

--- controllers/shop.js
+++ controllers/shop.js
@@ -6,22 +6,39 @@ const PDFDocument = require('pdfkit');
 const Product = require('../models/product');
 const Order = require('../models/order');
 
+const ITEMS_PER_PAGE = 2;
+
 exports.getProducts = (req, res, next) => {
+    const page = +req.query.page || 1;
+    let totalItems;
+  
     Product.find()
-    .then(products => {
-        console.log(products);
+      .countDocuments()
+      .then(numProducts => {
+        totalItems = numProducts;
+        return Product.find()
+          .skip((page - 1) * ITEMS_PER_PAGE)
+          .limit(ITEMS_PER_PAGE);
+      })
+      .then(products => {
         res.render('shop/product-list', {
-            prods: products,
-            docTitle: 'All Products',
-            path: '/products'
+          prods: products,
+          docTitle: 'Products',
+          path: '/products',
+          currentPage: page,
+          hasNextPage: ITEMS_PER_PAGE * page < totalItems,
+          hasPreviousPage: page > 1,
+          nextPage: page + 1,
+          previousPage: page - 1,
+          lastPage: Math.ceil(totalItems / ITEMS_PER_PAGE)
         });
-    })
-    .catch(err => {
+      })
+      .catch(err => {
         const error = new Error(err);
         error.httpStatusCode = 500;
         return next(error);
-    });
-};
+      });
+  };
 
 exports.getProduct = (req, res, next) => {
     const prodId = req.params.productId;
@@ -43,20 +60,36 @@ exports.getProduct = (req, res, next) => {
 
 
 exports.getIndex = (req, res, next) => {
+    const page = +req.query.page || 1;
+    let totalItems;
+  
     Product.find()
-    .then(products => {
+      .countDocuments()
+      .then(numProducts => {
+        totalItems = numProducts;
+        return Product.find()
+          .skip((page - 1) * ITEMS_PER_PAGE)
+          .limit(ITEMS_PER_PAGE);
+      })
+      .then(products => {
         res.render('shop/index', {
-            prods: products, 
-            docTitle: 'Shop', 
-            path:'/'
+          prods: products,
+          docTitle: 'Shop',
+          path: '/',
+          currentPage: page,
+          hasNextPage: ITEMS_PER_PAGE * page < totalItems,
+          hasPreviousPage: page > 1,
+          nextPage: page + 1,
+          previousPage: page - 1,
+          lastPage: Math.ceil(totalItems / ITEMS_PER_PAGE)
         });
-    })
-    .catch(err => {
+      })
+      .catch(err => {
         const error = new Error(err);
         error.httpStatusCode = 500;
         return next(error);
-    });
-};
+      });
+  };
 
 exports.getCart = (req, res, next) => {
     req.user
